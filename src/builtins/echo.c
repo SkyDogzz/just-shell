de7d4c530ebf169d6ab1b54617a9bf3bc5a8b061
Yanis Andry <yandry@student.42.fr>
2025-04-16 11:21:24 +0200

    add stub for echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,20 +6,15 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 16:50:50 by yandry            #+#    #+#             */
-/*   Updated: 2025/04/12 16:35:51 by yandry           ###   ########.fr       */
+/*   Updated: 2025/04/16 11:20:42 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// TODO: echo
 int	ft_echo(const t_cmd *cmd)
 {
-	int	i;
-
-	i = 2;
-	while (cmd->args[i])
-		ft_putstr_fd(cmd->args[i++], cmd->io[1]);
-	if (ft_strncmp(cmd->args[1], "-n", 2) == 0)
-		ft_putendl_fd("", cmd->io[1]);
+	(void)cmd;
 	return (0);
 }
