81a5aa8f9d0c710664651f68ca87eb02ed7c0af1
Yanis Andry <yandry@student.42.fr>
2025-04-17 17:10:08 +0200

    update env to be a linked list instead of an array (for the sake of simplicity

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 17:45:57 by yandry            #+#    #+#             */
-/*   Updated: 2025/04/14 19:05:38 by yandry           ###   ########.fr       */
+/*   Updated: 2025/04/17 16:40:21 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 # include "minishell.h"
 
-int		ft_exec_simple(const t_btree *root, char **env);
-int		ft_exec_pipeline(const t_btree *root, char **env);
+int		ft_exec_simple(const t_btree *root, t_list *env);
+int		ft_exec_pipeline(const t_btree *root, t_list *env);
 
-void	ft_subprocess(const t_cmd *cmd, char **env);
-void	ft_execft(const char *path, char *args[], char *env[]);
+void	ft_subprocess(const t_cmd *cmd, t_list *env);
+void	ft_execft(const char *path, char *args[], t_list *env);
 char	*ft_get_executable_path(const t_cmd *cmd);
 
 int		setup_pipe(int pipe_fds[2]);
