3e6eb6e5ca34500cee18dfa7c41c3fe5135d553f
Yanis Andry <yandry@student.42.fr>
2025-04-22 11:16:20 +0200

    ft_hack

--- src/execution/ft_execft.c
+++ src/execution/ft_execft.c
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 18:13:08 by yandry            #+#    #+#             */
-/*   Updated: 2025/04/21 19:04:18 by yandry           ###   ########.fr       */
+/*   Updated: 2025/04/22 17:18:51 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,7 @@ void	ft_execft(const char *path, char **args, t_list *env)
 			args,
 			(char *const *)ft_lsttoarray_c(env, copy_env_to_str)) == -1)
 	{
-		ft_putstr_fd("ssh-xx: failed to run ", 2);
-		ft_putendl_fd(args[0], 2);
+		ft_dprintf(STDERR_FILENO, COMMAND_FAILED, args[0]);
 		exit(127);
 	}
 }
@@ -46,9 +45,7 @@ void	ft_subprocess(const t_cmd *cmd, t_list *env)
 	path = ft_get_executable_path(cmd, env);
 	if (!path)
 	{
-		ft_putstr_fd("ssh-xx: command not found ('", 2);
-		ft_putstr_fd(cmd->args[0], 2);
-		ft_putendl_fd("')", 2);
+		ft_dprintf(STDERR_FILENO, COMMAND_NOT_FOUND, cmd->args[0]);
 		exit(128);
 	}
 	ft_execft(path, cmd->args, env);
