c4a241fff74ef7fc25c753b5bc7e3a1d03c759f7
skydogzz <tstephan00@outlook.com>
2025-04-22 19:40:26 +0200

    no leak

--- src/io/infile.c
+++ src/io/infile.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:09:09 by tstephan          #+#    #+#             */
-/*   Updated: 2025/04/22 19:16:41 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/04/22 19:39:13 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,20 @@ static void	ft_redir_heredoc(t_cmd *cmd, int fd)
 	ft_putstr_fd("\n", fd);
 }
 
-static void	ft_redir_input(t_cmd *cmd, int fd)
+static bool	ft_redir_input(t_cmd *cmd, int fd, char *filename)
 {
 	char	*buffer;
 	int		fd2;
 
 	fd2 = open(((t_redir *)cmd->redir->content)->file, O_RDONLY);
 	if (fd2 < 0)
-		return ;
-	if (fd2 <= 0)
 	{
 		ft_putstr_fd("ssh-xx: can't open file ('", 1);
 		ft_putstr_fd(((t_redir *)cmd->redir->content)->file, 1);
 		ft_putendl_fd("')", 1);
-		exit(127);
+		unlink(filename);
+		close(fd);
+		return (false);
 	}
 	buffer = ft_get_next_line(fd2);
 	while (buffer)
@@ -41,21 +41,26 @@ static void	ft_redir_input(t_cmd *cmd, int fd)
 		free(buffer);
 	}
 	close(fd2);
+	return (true);
 }
 
-static void	ft_redir_all(t_cmd *cmd, int fd)
+static bool	ft_redir_all(t_cmd *cmd, int fd, char *filename)
 {
 	if (((t_redir *)cmd->redir->content)->type != REDIR_INPUT
 		&& ((t_redir *)cmd->redir->content)->type != REDIR_HEREDOC)
 	{
 		cmd->redir = cmd->redir->next;
-		return ;
+		return (true);
 	}
 	if (((t_redir *)cmd->redir->content)->type == REDIR_INPUT)
-		ft_redir_input(cmd, fd);
+	{
+		if (!ft_redir_input(cmd, fd, filename))
+			return (false);
+	}
 	else
 		ft_redir_heredoc(cmd, fd);
 	cmd->redir = cmd->redir->next;
+	return (true);
 }
 
 void	ft_infile_exec(t_cmd *cmd)
@@ -67,12 +72,11 @@ void	ft_infile_exec(t_cmd *cmd)
 	{
 		filename = get_tmp_fd();
 		fd = open(filename, O_CREAT | O_WRONLY, 0644);
-		printf("lui = '%s'\n", filename);
-		printf("1 %d\n", fd);
 		if (fd < 0)
 			return ;
 		while (cmd->redir)
-			ft_redir_all(cmd, fd);
+			if (!ft_redir_all(cmd, fd, filename))
+				exit(127);
 		close(fd);
 		fd = open(filename, O_RDONLY);
 		if (fd < 0)
@@ -80,5 +84,6 @@ void	ft_infile_exec(t_cmd *cmd)
 		dup2(fd, STDIN_FILENO);
 		close(fd);
 		unlink(filename);
+		free(filename);
 	}
 }
