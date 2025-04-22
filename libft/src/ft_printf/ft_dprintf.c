e284bc32354abe2e9dc138fde90f131c2c375315
Yanis Andry <yandry@student.42.fr>
2025-04-22 11:16:20 +0200

    ft_hack

--- libft/src/ft_printf/ft_dprintf.c
+++ libft/src/ft_printf/ft_dprintf.c
@@ -6,14 +6,12 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 20:07:29 by yandry            #+#    #+#             */
-/*   Updated: 2025/04/21 20:13:12 by yandry           ###   ########.fr       */
+/*   Updated: 2025/04/22 11:16:01 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
-
-static int	g_fd;
 
 void	out_fd(	char character,
 			void *buffer,
@@ -22,12 +20,9 @@ void	out_fd(	char character,
 {
 	(void)buffer;
 	(void)index;
-	(void)n;
 	if (character)
-		ft_putchar_fd(character, g_fd);
-	
+		ft_putchar_fd(character, (int)n);
 }
-
 
 int	ft_dprintf(int fd, const char *format, ...)
 {
@@ -39,14 +34,12 @@ int	ft_dprintf(int fd, const char *format, ...)
 	if (!format)
 		return (-1);
 	va_start(args, format);
-	g_fd = fd;
 	int_printf.format = (char *)format;
 	int_printf.buffer = buff;
-	int_printf.max_len = (size_t)-1;
+	int_printf.max_len = fd;
 	int_printf.out = out_fd;
 	va_copy(int_printf.va, args);
 	len = ft_vsnprintf(int_printf);
 	va_end(args);
 	return ((int)len);
-
 }
