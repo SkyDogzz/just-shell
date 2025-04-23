5c09b744cd488ffccc37900c2782a73c847ec95a
Yanis Andry <yandry@student.42.fr>
2025-04-23 13:46:58 +0200

    add unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 13:31:11 by yandry            #+#    #+#             */
-/*   Updated: 2025/04/23 13:33:54 by yandry           ###   ########.fr       */
+/*   Updated: 2025/04/23 13:46:38 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 int	ft_unset(const t_cmd *cmd, t_list *env)
 {
-	(void)cmd;
-	(void)env;
+	int	i;
+
+	if (!cmd->args[1])
+	{
+		ft_dprintf(STDERR_FILENO, "unset: need at least 1 argument\n");
+		return (1);
+	}
+	i = 1;
+	while (cmd->args[i])
+		ft_delete_env(&env, cmd->args[i++]);
 	return (0);
 }
