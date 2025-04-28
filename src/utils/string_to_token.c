dc2925d3c57b0b10d1b0cd7047aed6c80cfde599
Thomas Stephan <tstephan@paul-f4Ar1s1.clusters.42paris.fr>
2025-04-28 16:16:09 +0200

    norm

--- src/utils/string_to_token.c
+++ src/utils/string_to_token.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:02:11 by tstephan          #+#    #+#             */
-/*   Updated: 2025/04/28 14:04:02 by skydogzz         ###   ########.fr       */
+/*   Updated: 2025/04/28 16:15:47 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,28 @@ static t_token_type	ft_gettype(const char *s)
 	return (T_WORD);
 }
 
+static bool	create_token(t_list **tokens, struct dirent *file)
+{
+	t_token			*new;
+	t_token			*new_space;
+
+	new = (t_token *)malloc(sizeof(t_token));
+	new_space = (t_token *)malloc(sizeof(t_token));
+	if (!new || !new_space)
+		return (false);
+	new->content = ft_strdup(file->d_name);
+	new->token_type = T_WORD;
+	ft_lstadd_back(tokens, ft_lstnew(new));
+	new_space->content = ft_strdup(" ");
+	new_space->token_type = T_BLANK;
+	ft_lstadd_back(tokens, ft_lstnew(new_space));
+	return (true);
+}
+
 bool	ft_expand_wildcard(t_list **tokens, t_token *dup)
 {
 	DIR				*dir;
 	struct dirent	*file;
-	t_token			*new;
-	t_token			*new_space;
 
 	if (dup->token_type != T_WORD || strcmp(dup->content, "*") != 0)
 		return (false);
@@ -51,54 +67,51 @@ bool	ft_expand_wildcard(t_list **tokens, t_token *dup)
 			file = readdir(dir);
 			continue ;
 		}
-		new = (t_token *)malloc(sizeof(t_token));
-		new_space = (t_token *)malloc(sizeof(t_token));
-		if (!new || !new_space)
+		if (!create_token(tokens, file))
 		{
 			closedir(dir);
 			return (false);
 		}
-		new->content = ft_strdup(file->d_name);
-		new->token_type = T_WORD;
-		ft_lstadd_back(tokens, ft_lstnew(new));
-		new_space->content = ft_strdup(" ");
-		new_space->token_type = T_BLANK;
-		ft_lstadd_back(tokens, ft_lstnew(new_space));
 		file = readdir(dir);
 	}
 	closedir(dir);
 	return (true);
 }
 
+static void	ft_expand_utils(t_list *env, t_list **tokens, t_list *act)
+{
+	t_token	*dup;
+
+	dup = malloc(sizeof(t_token));
+	if (!dup)
+		return ;
+	dup->content = ft_strdup(act->content);
+	dup->token_type = ft_gettype(dup->content);
+	if (ft_expand_wildcard(tokens, dup))
+	{
+		free(dup->content);
+		free(dup);
+		return ;
+	}
+	dup = ft_expand(env, dup);
+	dup = ft_remove_quote(dup);
+	if (ft_strcmp(dup->content, "") != 0)
+		ft_lstadd_back(tokens, ft_lstnew(dup));
+	else
+	{
+		free(dup->content);
+		free(dup);
+	}
+}
+
 t_list	*ft_string_to_token(t_list *env, t_list *tokens, t_list *pre_tokens)
 {
 	t_list	*act;
-	t_token	*dup;
 
 	act = pre_tokens;
 	while (act)
 	{
-		dup = malloc(sizeof(t_token));
-		if (!dup)
-			return (tokens);
-		dup->content = ft_strdup(act->content);
-		dup->token_type = ft_gettype(dup->content);
-		if (ft_expand_wildcard(&tokens, dup))
-		{
-			free(dup->content);
-			free(dup);
-			act = act->next;
-			continue ;
-		}
-		dup = ft_expand(env, dup);
-		dup = ft_remove_quote(dup);
-		if (ft_strcmp(dup->content, "") != 0)
-			ft_lstadd_back(&tokens, ft_lstnew(dup));
-		else
-		{
-			free(dup->content);
-			free(dup);
-		}
+		ft_expand_utils(env, &tokens, act);
 		act = act->next;
 	}
 	return (tokens);
