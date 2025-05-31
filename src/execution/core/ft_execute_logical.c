cda1757df59b946a6caaa54c0a6def27763476c4
Yanis Andry <yandry@student.42.fr>
2025-05-31 08:55:13 +0200

    free context in exec logical

--- src/execution/core/ft_execute_logical.c
+++ src/execution/core/ft_execute_logical.c
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:14:14 by yandry            #+#    #+#             */
-/*   Updated: 2025/05/27 14:57:30 by yandry           ###   ########.fr       */
+/*   Updated: 2025/05/31 08:54:44 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ static int	exec_logical_node(t_btree *node, t_list *env)
 	context_left = ft_get_execution_context(node->left, env);
 	context_right = ft_get_execution_context(node->right, env);
 	ft_exec(context_left);
+	ft_free_context(context_left, false);
 	if (g_exit == 1)
-		return (-1);
+		return (ft_free_context(context_right, false), -1);
 	if (g_exit != 0 && ft_strcmp(((t_leaf *)node->content)->cmd->args[0], "or")
 		== 0)
 		ret = ft_exec(context_right);
@@ -30,8 +31,8 @@ static int	exec_logical_node(t_btree *node, t_list *env)
 			"and") == 0)
 		ret = ft_exec(context_right);
 	else
-		return (-1);
-	return (ret);
+		return (ft_free_context(context_right, false), -1);
+	return (ft_free_context(context_right, false), ret);
 }
 
 int	ft_exec_logical(const t_context *context)
