a53a23fab85d9a67e1451d335a348f5f7b3d1397
Thomas Stephan <tstephan@f6r3s3.clusters.42paris.fr>
2025-06-09 18:23:44 +0200

    comment cela se fut-ce

--- includes/ft_execution.h
+++ includes/ft_execution.h
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 17:45:57 by yandry            #+#    #+#             */
-/*   Updated: 2025/05/29 12:40:15 by yandry           ###   ########.fr       */
+/*   Updated: 2025/06/09 18:02:27 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,31 +34,15 @@ typedef struct s_context
 t_context	*ft_get_execution_context(t_btree *tree, t_list *env);
 void		ft_free_context(t_context *context, bool clear_env);
 
-int			ft_exec(const t_context *const context);
+int			ft_exec(t_context *context);
 
 bool		ft_is_builtin(const char *name);
 bool		ft_cmd_exists(t_cmd *cmd, t_list *env);
 int			ft_execute_builtin(const t_cmd *cmd, t_list *env);
 
-int			ft_exec_simple(const t_context *const context);
-int			ft_exec_pipeline(const t_context *const context);
-int			ft_exec_logical(const t_context *const context);
-int			ft_exec_with_redirects(t_cmd *cmd,
-				t_list *env,
-				int fd_in,
-				int fd_out);
-
-void		ft_subprocess(t_cmd *cmd, t_list *env);
-void		ft_execft(const char *path, char *args[], t_list *env);
 char		*ft_get_executable_path(const t_cmd *cmd, t_list *env);
 
 int			setup_pipe(int pipe_fds[2]);
 void		destop_turbo(int pipe_fds[2]);
 
-int			exec_left_child(t_btree *root, t_context *context,
-				int fd_in, int pipe_fds[2]);
-int			handle_right_word_node(t_context *context, int pipe_fd,
-				pid_t left_pid);
-int			handle_right_node(t_context *context, int pipe_fd,
-				pid_t left_pid);
 #endif
