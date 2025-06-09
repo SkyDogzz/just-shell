c79000897a4c38cc7fe4e98fd3cbbb1e2ebe12ab
skydogzz <tstephan00@outlook.com>
2025-06-09 20:37:14 +0200

    il se fait froid

--- src/execution/ft_exec.c
+++ src/execution/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 18:15:59 by yandry            #+#    #+#             */
-/*   Updated: 2025/06/09 18:22:20 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/09 20:36:31 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,48 @@
 #include "minishell.h"
 #include <unistd.h>
 
+static int	ft_freeplusplus(char *path, void ***env_arr)
+{
+	if (path)
+		free(path);
+	if (env_arr)
+		ft_free_array(env_arr);
+	return (127);
+}
+
+static int	ft_sombrax(t_cmd *cmd, t_list *env)
+{
+	char	*path;
+	void	**env_arr;
+
+	if (!ft_cmd_exists(cmd, env))
+		return (127);
+	if (ft_is_builtin(cmd->args[0]))
+		return (ft_execute_builtin(cmd, env));
+	path = ft_get_executable_path(cmd, env);
+	if (!path)
+		return ft_freeplusplus(path, NULL);
+	env_arr = ft_lsttoarray_c(env, copy_env_to_str);
+	if (!env_arr)
+		return ft_freeplusplus(path, NULL);
+	if (!execve(path, cmd->args, (char * const *)env_arr))
+		return ft_freeplusplus(path, &env_arr);
+	return (0);
+}
+
 static int	ft_exec_simple(t_context *context)
 {
-	printf("Exec simple commands\n");
-	return (0);
-	(void) context;
+	pid_t	pid;
+	int		ret;
+
+	ret = 0;
+	if (!ft_infile_exec(((t_leaf *)(context->root)->content)->cmd))
+		return (127);
+	pid = fork();
+	if (pid == 0)
+		ret = ft_sombrax(((t_leaf *)(context->root->content))->cmd, context->env);
+	waitpid(pid, &ret, 0);
+	return (ret);
 }
 
 static int	ft_exec_pipeline(t_context *context)
