30bf888a6f8178fb6575b5d132846692f1a268d6
Thomas Stephan <tstephan@f1br6s3.clusters.42paris.fr>
2025-06-19 06:40:29 +0200

    oupsi mb

--- src/execution/ft_exec_logical.c
+++ src/execution/ft_exec_logical.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 17:26:09 by tstephan          #+#    #+#             */
-/*   Updated: 2025/06/19 03:10:55 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/19 06:36:50 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ static int	exec_logical_node(t_btree *node, t_list *env)
 
 	context_left = ft_get_execution_context(node->left, env);
 	context_right = ft_get_execution_context(node->right, env);
-	node_ret = ft_exec(context_left, true);
+	node_ret = ft_exec(context_left);
 	free(context_left);
 	if (node_ret == 1)
 		return (ft_free_context(context_right, false), -1);
 	if (node_ret != 0
 		&& ft_strcmp(((t_leaf *)node->content)->cmd->args[0], "or") == 0)
-		ret = ft_exec(context_right, true);
+		ret = ft_exec(context_right);
 	else if (node_ret == 0 && ft_strcmp(((t_leaf *)node->content)->cmd->args[0],
 			"and") == 0)
-		ret = ft_exec(context_right, true);
+		ret = ft_exec(context_right);
 	else
 		return (free(context_right), -1);
 	return (free(context_right), ret);
