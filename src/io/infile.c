28610e9845a780b83232b6ebfd51f222d6045cd2
skydogzz <tstephan00@outlook.com>
2025-06-19 12:39:49 +0200

    mo

--- src/io/infile.c
+++ src/io/infile.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:09:09 by tstephan          #+#    #+#             */
-/*   Updated: 2025/06/19 12:21:45 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/19 12:37:53 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ static bool	ft_redir_input(t_cmd *cmd, int fd, char *filename)
 	return (true);
 }
 
-static bool	ft_redir_all(t_cmd *cmd, int fd, char *filename)
+bool	ft_redir_all(t_cmd *cmd, int fd, char *filename)
 {
 	if (((t_redir *)cmd->redir->content)->type != REDIR_INPUT
 		&& ((t_redir *)cmd->redir->content)->type != REDIR_HEREDOC)
@@ -78,45 +78,5 @@ static bool	ft_redir_all(t_cmd *cmd, int fd, char *filename)
 	else
 		ft_redir_heredoc(cmd, fd);
 	cmd->redir = cmd->redir->next;
-	return (true);
-}
-
-bool	ft_infile_exec(t_cmd *cmd)
-{
-	int		fd;
-	char	*filename;
-	t_list	*redir;
-
-	redir = cmd->redir;
-	if (cmd->redir)
-	{
-		filename = get_tmp_fd();
-		fd = open(filename, O_CREAT | O_WRONLY | O_CLOEXEC, 0644);
-		if (fd < 0)
-		{
-			free(filename);
-			return (false);
-		}
-		while (cmd->redir)
-		{
-			if (!ft_redir_all(cmd, fd, filename))
-			{
-				free(filename);
-				return (false);
-			}
-		}
-		ft_close(&fd);
-		fd = open(filename, O_RDONLY | O_CLOEXEC);
-		if (fd < 0)
-		{
-			free(filename);
-			return (false);
-		}
-		dup2(fd, STDIN_FILENO);
-		ft_close(&fd);
-		unlink(filename);
-		free(filename);
-	}
-	cmd->redir = redir;
 	return (true);
 }
---
+++ src/io/infile_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   infile_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/19 12:36:42 by tstephan          #+#    #+#             */
+/*   Updated: 2025/06/19 12:36:57 by tstephan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "ft_io.h"
+
+static int	open_tmp_write(char **filename)
+{
+	int	fd;
+
+	*filename = get_tmp_fd();
+	fd = open(*filename, O_CREAT | O_WRONLY | O_CLOEXEC, 0644);
+	if (fd < 0)
+	{
+		free(*filename);
+		return (-1);
+	}
+	return (fd);
+}
+
+static bool	handle_redirections(t_cmd *cmd, int fd, char *filename)
+{
+	while (cmd->redir)
+	{
+		if (!ft_redir_all(cmd, fd, filename))
+			return (false);
+	}
+	return (true);
+}
+
+static bool	replace_stdin_with_tmp(char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_RDONLY | O_CLOEXEC);
+	if (fd < 0)
+		return (false);
+	dup2(fd, STDIN_FILENO);
+	ft_close(&fd);
+	return (true);
+}
+
+bool	ft_infile_exec(t_cmd *cmd)
+{
+	int		fd;
+	char	*filename;
+	t_list	*redir;
+
+	redir = cmd->redir;
+	if (!cmd->redir)
+		return (true);
+	fd = open_tmp_write(&filename);
+	if (fd < 0)
+		return (false);
+	if (!handle_redirections(cmd, fd, filename))
+	{
+		free(filename);
+		return (false);
+	}
+	ft_close(&fd);
+	if (!replace_stdin_with_tmp(filename))
+	{
+		free(filename);
+		return (false);
+	}
+	unlink(filename);
+	free(filename);
+	cmd->redir = redir;
+	return (true);
+}
