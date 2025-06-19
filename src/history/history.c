04d8f4b735cbc2566e883013cc4c419e4b343c55
skydogzz <tstephan00@outlook.com>
2025-06-19 02:34:41 +0200

    no more crash when minishell dont have env

--- src/history/history.c
+++ src/history/history.c
@@ -6,11 +6,23 @@
 /*   By: skydogzz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 06:40:36 by skydogzz          #+#    #+#             */
-/*   Updated: 2025/06/11 16:04:43 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/19 01:32:23 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_history.h"
+
+static char	*get_path(t_list *env)
+{
+	char	*path;
+	t_env	*home;
+
+	home = ft_get_env(env, "HOME");
+	if (!home)
+		return (NULL);
+	path = ft_strreplace(HISTORY_FILE, "~", home->value);
+	return (path);
+}
 
 void	retreive_history(t_list *env)
 {
@@ -19,7 +31,7 @@ void	retreive_history(t_list *env)
 	char	*path;
 	char	*trimmed;
 
-	path = ft_strreplace(HISTORY_FILE, "~", ft_get_env(env, "HOME")->value);
+	path = get_path(env);
 	if (!path)
 		return ;
 	fd = open(path, O_RDONLY | O_CLOEXEC);
@@ -47,7 +59,7 @@ void	store_history(char *input, t_list *env)
 	int			fd;
 
 	add_history(input);
-	path = ft_strreplace(HISTORY_FILE, "~", ft_get_env(env, "HOME")->value);
+	path = get_path(env);
 	if (!path)
 		return ;
 	fd = open(path, O_WRONLY | O_APPEND | O_CREAT | O_CLOEXEC, 0644);
