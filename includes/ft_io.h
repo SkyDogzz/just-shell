f95f443edecb4f7669f7070f98d267e359c9ff51
Thomas Stephan <tstephan@f1br6s3.clusters.42paris.fr>
2025-06-19 12:39:49 +0200

    mo

--- includes/ft_io.h
+++ includes/ft_io.h
@@ -6,7 +6,7 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 12:48:08 by yandry            #+#    #+#             */
-/*   Updated: 2025/06/19 06:18:56 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/19 12:39:00 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define FT_IO_H
 
 # include <stdbool.h>
+# include "minishell.h"
 
 bool	ft_close(int *fd);
 void	init_fds(int fd[5]);
 int		dup2v2(int fd, int fd2);
 int		pipev2(int *pipedes);
+bool	ft_redir_all(t_cmd *cmd, int fd, char *filename);
 
 #endif
