218d2812bf5b400e3dbed993fc0a896310252988
Yanis Andry <yandry@student.42.fr>
2025-06-21 14:53:21 +0200

    improve prompt signals

--- src/prompt/prompt_exit_utils.c
+++ src/prompt/prompt_exit_utils.c
@@ -6,32 +6,13 @@
 /*   By: yandry <yandry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 21:56:13 by yandry            #+#    #+#             */
-/*   Updated: 2025/06/20 22:26:35 by yandry           ###   ########.fr       */
+/*   Updated: 2025/06/21 14:51:50 by yandry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_prompt.h"
 #include "minishell.h"
 #include "ft_signals.h"
-
-const char	*get_signal_name(int sig_no)
-{
-	const t_signal	*sig = g_signals;
-	static char		rt_buffer[16];
-
-	if (sig_no >= SIGRTMIN && sig_no <= SIGRTMAX)
-	{
-		ft_snprintf(rt_buffer, sizeof(rt_buffer),
-			"SIGRT_%d", sig_no - SIGRTMIN);
-		return (rt_buffer);
-	}
-	while (sig->sig_name)
-	{
-		if (sig->sig_no == sig_no)
-			return (sig->sig_name);
-		sig++;
-	}
-	return ("N/A");
-}
 
 const char	*get_exited_status(int status)
 {
@@ -50,33 +31,36 @@ const char	*get_exited_status(int status)
 	return (last_status);
 }
 
-const char	*get_signaled_status(int status)
+const char	*get_signaled_status(int status, bool include_desc)
 {
-	char		*exit;
-	char		*temp;
-	char		*failure;
-	const char	*last_status;
+	const t_sig_pair	*signal;
+	char				*buffer;
 
-	exit = ft_strdup(get_signal_name(WTERMSIG(status)));
-	failure = ft_strjoin(ORANGE"🗲 - ", exit);
-	temp = ft_strdup(failure);
-	free(failure);
-	free(exit);
-	last_status = ft_strjoin(temp, NC);
-	free(temp);
-	return (last_status);
+	signal = get_signal(WTERMSIG(status));
+	if (!signal)
+		return (ft_strdup(ORANGE SIGNAL_NONE NC));
+	buffer = ft_calloc(sizeof(char), 64);
+	if (!buffer)
+		return (ft_strdup(ORANGE SIGNAL_NONE NC));
+	if (include_desc)
+		ft_snprintf(buffer, 64, ORANGE SIGNAL_WITH_DESC NC,
+			signal->signal_name, signal->signal_description);
+	else
+		ft_snprintf(buffer, 64, ORANGE SIGNAL_SIMPLE NC, signal->signal_name);
+	free((void *)signal);
+	return (buffer);
 }
 
-const char	*get_prompt_last_exit(int last_exit)
+const char	*get_prompt_last_exit(int last_exit, bool include_desc)
 {
 	const char	*last_status;
 
-	if (WEXITSTATUS(last_exit << 8) == 0)
+	if (WEXITSTATUS(last_exit) == 0 && !WIFSIGNALED(last_exit))
 		return (ft_strdup(GREEN"✓"NC));
 	else if (WIFEXITED(last_exit))
 		last_status = get_exited_status(last_exit);
 	else if (WIFSIGNALED(last_exit))
-		last_status = get_signaled_status(last_exit);
+		last_status = get_signaled_status(last_exit, include_desc);
 	else if (last_exit & CMD_NOT_FOUND_FLAG)
 		last_status = ft_strdup(RED"✗ - not found"NC);
 	else if (last_exit & CANT_OPEN_OUTFILE)
