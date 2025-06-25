bfa054ec96bd43d2d0fbec2c1e9e804d38843ab9
Thomas Stephan <tstephan@f2r7s18.clusters.42paris.fr>
2025-06-25 20:52:45 +0200

    c bo

--- src/execution/ft_pipeline_utils.c
+++ src/execution/ft_pipeline_utils.c
@@ -6,12 +6,52 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 18:27:45 by tstephan          #+#    #+#             */
-/*   Updated: 2025/06/25 20:44:10 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/25 20:51:14 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_execution.h"
 #include "ft_io.h"
+
+static int	get_fd(t_redir *redir)
+{
+	int	flags;
+	int	newfd;
+
+	flags = 0;
+	if (redir->type == REDIR_TRUNC_STDOUT)
+		flags = O_CREAT | O_WRONLY | O_TRUNC;
+	else if (redir->type == REDIR_APPEND_STDOUT)
+		flags = O_CREAT | O_WRONLY | O_APPEND;
+	else if (redir->type == REDIR_TRUNC_STDERR)
+		flags = O_CREAT | O_WRONLY | O_TRUNC;
+	else if (redir->type == REDIR_APPEND_STDERR)
+		flags = O_CREAT | O_WRONLY | O_APPEND;
+	else if (redir->type == REDIR_TRUNC_STDALL)
+		flags = O_CREAT | O_WRONLY | O_TRUNC;
+	else if (redir->type == REDIR_APPEND_STDALL)
+		flags = O_CREAT | O_WRONLY | O_APPEND;
+	newfd = open(redir->file, flags, 0644);
+	return (newfd);
+}
+
+static void	dup_fd(t_redir *redir, int newfd)
+{
+	if (redir->type == REDIR_INPUT)
+		dup2v2(newfd, STDIN_FILENO);
+	else if (redir->type == REDIR_TRUNC_STDOUT
+		|| redir->type == REDIR_APPEND_STDOUT)
+		dup2v2(newfd, STDOUT_FILENO);
+	else if (redir->type == REDIR_TRUNC_STDERR
+		|| redir->type == REDIR_APPEND_STDERR)
+		dup2v2(newfd, STDERR_FILENO);
+	else if (redir->type == REDIR_TRUNC_STDALL
+		|| redir->type == REDIR_APPEND_STDALL)
+	{
+		dup2v2(newfd, STDOUT_FILENO);
+		dup2v2(newfd, STDERR_FILENO);
+	}
+}
 
 int	manage_redir_child(t_contex2 *context)
 {
@@ -19,7 +59,6 @@ int	manage_redir_child(t_contex2 *context)
 	t_list	*r;
 	t_redir	*redir;
 	int		newfd;
-	int		flags;
 
 	leaf = context->context->content;
 	r = leaf->cmd->redir;
@@ -29,38 +68,10 @@ int	manage_redir_child(t_contex2 *context)
 		if (redir->type == REDIR_INPUT || redir->type == REDIR_HEREDOC)
 			newfd = open(redir->file, O_RDONLY);
 		else
-		{
-			flags = 0;
-			if (redir->type == REDIR_TRUNC_STDOUT)
-				flags = O_CREAT | O_WRONLY | O_TRUNC;
-			else if (redir->type == REDIR_APPEND_STDOUT)
-				flags = O_CREAT | O_WRONLY | O_APPEND;
-			else if (redir->type == REDIR_TRUNC_STDERR)
-				flags = O_CREAT | O_WRONLY | O_TRUNC;
-			else if (redir->type == REDIR_APPEND_STDERR)
-				flags = O_CREAT | O_WRONLY | O_APPEND;
-			else if (redir->type == REDIR_TRUNC_STDALL)
-				flags = O_CREAT | O_WRONLY | O_TRUNC;
-			else if (redir->type == REDIR_APPEND_STDALL)
-				flags = O_CREAT | O_WRONLY | O_APPEND;
-			newfd = open(redir->file, flags, 0644);
-		}
+			newfd = get_fd(redir);
 		if (newfd < 0)
 			return (127);
-		if (redir->type == REDIR_INPUT)
-			dup2v2(newfd, STDIN_FILENO);
-		else if (redir->type == REDIR_TRUNC_STDOUT
-			|| redir->type == REDIR_APPEND_STDOUT)
-			dup2v2(newfd, STDOUT_FILENO);
-		else if (redir->type == REDIR_TRUNC_STDERR
-			|| redir->type == REDIR_APPEND_STDERR)
-			dup2v2(newfd, STDERR_FILENO);
-		else if (redir->type == REDIR_TRUNC_STDALL
-			|| redir->type == REDIR_APPEND_STDALL)
-		{
-			dup2v2(newfd, STDOUT_FILENO);
-			dup2v2(newfd, STDERR_FILENO);
-		}
+		dup_fd(redir, newfd);
 		close(newfd);
 		r = r->next;
 	}
