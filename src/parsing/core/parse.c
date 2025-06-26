5d12fd4349958413248a86cc9274c23164b2658b
skydogzz <tstephan00@outlook.com>
2025-06-26 03:43:11 +0200

    no need to check cmd (we do it later)

--- src/parsing/core/parse.c
+++ src/parsing/core/parse.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 20:12:18 by tstephan          #+#    #+#             */
-/*   Updated: 2025/06/09 18:22:36 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/06/26 03:39:57 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,22 +61,6 @@ static t_list	*parse_pipe(t_list *tokens)
 	return (pipes);
 }
 
-static bool	ft_verif_cmd(t_btree *root)
-{
-	t_leaf	*leaf;
-	bool	status;
-
-	if (!root || !root->content)
-		return (true);
-	leaf = root->content;
-	if (leaf->type == NODE_WORD && !leaf->cmd->args[0])
-		return (false);
-	status = ft_verif_cmd(root->left);
-	if (status)
-		ft_verif_cmd(root->right);
-	return (status);
-}
-
 t_btree	*ft_parse(t_list *tokens)
 {
 	t_btree	*root;
@@ -86,36 +70,5 @@ t_btree	*ft_parse(t_list *tokens)
 	root = NULL;
 	ft_fill_tree(&root, pipes);
 	ft_lstclear(&pipes, ft_lstclear_pipes);
-	if (!ft_verif_cmd(root))
-	{
-		ft_btree_clear(&root, ft_free_leaf);
-		ft_putendl_fd("Invalid command", STDERR_FILENO);
-		return (NULL);
-	}
 	return (root);
 }
-
-/*t_btree	*ft_parse(t_list *tokens)*/
-/*{*/
-/*	t_btree	*root;*/
-/**/
-/*	root = ft_btree_new(ft_create_leaf(1,*/
-/*				ft_split("first", ' ')));*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(2,*/
-/*				ft_split("second", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(3,*/
-/*				ft_split("third", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(0,*/
-/*				ft_split("fourth", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(0,*/
-/*				ft_split("fifth", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(0,*/
-/*				ft_split("sixth", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(0,*/
-/*				ft_split("seventh", ' '))), ft_cmp_leaf);*/
-/*	ft_btree_insert(&root, ft_btree_new(ft_create_leaf(0,*/
-/*				ft_split("eigth", ' '))), ft_cmp_leaf);*/
-/*	ft_print_tree(root, 0, 0);*/
-/*	return (root);*/
-/*	(void) tokens;*/
-/*}*/
